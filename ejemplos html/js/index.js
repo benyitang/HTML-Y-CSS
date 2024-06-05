da992270ca853f3a458190503043ccecd1e5e5bd
benyitang <163897566+benyitang@users.noreply.github.com>
2024-06-05 14:27:17 -0300

    Create index.js

---
+++ ejemplos html/js/index.js
@@ -0,0 +1,9 @@
+function evento(event) {
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.width = '100px';
+    });
+
+    let currentBox = event.currentTarget;
+    currentBox.style.width = '600px';
+}
